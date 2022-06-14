5fbe67fa4ac22400e8a3bf2e76d35aa05923ef20
hermano <hermano_assis@hotmail.com>
2022-06-14 14:40:12 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,8 @@
+let meuNome = "Hermano"
+let meuSobrenome = "de Assis Filho"
+let anoNascimento = 2001
+let cidade = "Taubaté"
+let anoAtual = 2022
+
+let fraseCompleta = `Eu sou o ${meuNome} ${meuSobrenome}, tenho ${anoAtual - anoNascimento} e moro em ${cidade}`
+console.log(fraseCompleta)
